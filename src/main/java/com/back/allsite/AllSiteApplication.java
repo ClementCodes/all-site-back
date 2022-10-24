94b9e626ebc2519e94430509577d66216e7cda29
Clément <79195642+ClementCodes@users.noreply.github.com>
2022-10-24 15:06:18 +0200

    first commit

---
+++ src/main/java/com/back/allsite/AllSiteApplication.java
@@ -0,0 +1,13 @@
+package com.back.allsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllSiteApplication.class, args);
+	}
+
+}
